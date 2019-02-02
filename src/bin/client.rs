b95716446de2a6dbdb03418644907586c40023c6
Anecoz <heilmair5@gmail.com>
2019-02-02 13:33:59 +0100

    Working one-way communication

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,3 +1,40 @@
+use std::net::TcpStream;
+use std::io::{self, BufReader, BufRead, BufWriter, Write};
+use std::thread;
+
+fn handle_receiving(stream: TcpStream)
+{
+  thread::spawn(move || {
+    let mut reader = BufReader::new(&stream);
+    let mut line = String::with_capacity(512);
+    loop {
+      let result = reader.read_line(&mut line);
+      match result {
+        Ok(_) => {
+          println!("Received from server: {}", line);
+        },
+        Err(e) => panic!("Could not read to string {}", e),
+      }
+    }
+  });
+}
+
 fn main() {
-  println!("Hello from Client!");
+  let stream = match TcpStream::connect("127.0.0.1:8888") {
+    Ok(stream) => stream,
+    Err(e) => panic!("Could not connect: {}", e),
+  };
+
+  handle_receiving(stream.try_clone().expect("Could not clone stream"));
+
+  println!("Connected.");
+
+  let mut writer = BufWriter::new(&stream);  
+  loop {
+    let mut line = String::new();
+    io::stdin().read_line(&mut line).expect("Failed to read line");
+
+    writer.write(line.as_bytes()).expect("Could not write line to stream");
+    writer.flush().expect("Could not flush");
+  }
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,3 +1,62 @@
+//use std::sync::mpsc;
+use std::thread;
+use std::io::{BufReader, BufWriter, Write, BufRead};
+use std::net::{TcpListener, TcpStream};
+
+fn handle_new_connection(stream: TcpStream) {
+  thread::spawn(move || {
+    let mut reader = BufReader::new(&stream);
+    let mut writer = BufWriter::new(&stream);
+
+    // Greet the new connection with a welcome message.
+    writer.write(b"Welcome to Uganda, friend!\n").expect("Could not write!");
+    writer.flush().expect("Could not flush!");
+
+    // Loop and wait for the connection's messages.    
+    loop {
+      let mut line = String::new();
+      let result = reader.read_line(&mut line);
+      match result {
+        Ok(_) => (),
+        Err(e) => panic!("Could not read to string {}", e),
+      }
+
+      if line.len() > 0 {
+        print!("Received from connection: {}", line);
+      }
+    }
+  });
+}
+
 fn main() {
-    println!("Hello, world from server!");
+  println!("Starting server on port 8888");
+
+  let listener = match TcpListener::bind("127.0.0.1:8888") {
+    Ok(listener) => listener,
+    Err(error) => panic!(error),
+  };
+
+  for stream in listener.incoming() {
+    match stream {
+      Ok(stream) => {
+        println!("Received new connection.");
+        handle_new_connection(stream);
+      },
+      Err(e) => panic!("Received error while accepting connections: {}", e),
+    }
+  }
+
+  /*let (tx, rx) = mpsc::channel();
+
+  thread::spawn(move || {
+    let val = String::from("hi!");
+    match tx.send(val) {
+      Ok(()) => (),
+      Err(string) => println!("Send error: {}", string),
+    }
+  });
+
+  for r in rx {
+    println!("Received: {}", r);
+  }*/
 }
